65620513e960b13cab1c8474b0275e85f3d36b87
sin <yasinai@gmail.com>
2016-07-17 16:54:07 +0300

    Added connection to vmrack + GET-VM command.

--- get_cluster_info.ps1
+++ get_cluster_info.ps1
@@ -1,9 +1,21 @@
-$vcServer = "vc-vmrack"
-$cluster = "VMRACKS"
-$esxCred = Get-Credential
-Connect-VIServer $vcServer | Out-Null
+#$esxCred = Get-Credential
+
+
+Add-PSSnapin VMware.VimAutomation.Core
+
+#Connecting to VC-VMRACK
+Connect-VIServer VC-VMRACK -user $domainuser -Password $domainpassword | Out-Null
+
+
+#Get List of VMs
+
+Get-VM | Get-CDDrive | select @{N="VM";E="Parent"},IsoPath | where {$_.IsoPath -ne $null}   | Select-String -Pattern "scratch"  | format-list -property Line | out-file c:\jenkins\test.csv
+
+
+
 #Connect to ESX hosts in cluster
-foreach ($esx in Get-Cluster $cluster | Get-VMHost) {
-Connect-VIServer $esx -Credential $esxCred | Out-Null
-Get-VMHostAdvancedConfiguration -Name "ScratchConfig.ConfiguredScratchLocation"
-}
+
+#foreach ($esx in Get-Datacenter VMRACKS | Get-VMHost) {
+#Connect-VIServer $esx -Credential $esxCred | Out-Null
+#Get-VMHostAdvancedConfiguration -Name "ScratchConfig.ConfiguredScratchLocation"
+#}
